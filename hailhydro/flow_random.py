241272983778c0a3d58c53a76ab98d667b3e8472
Felix <felixuwekramer@protonmail.com>
2021-09-14 00:05:06 +0200

    update random flow draw

--- hailhydro/flow_random.py
+++ hailhydro/flow_random.py
@@ -3,7 +3,7 @@
 # @Email:  kramer@mpi-cbg.de
 # @Project: go-with-the-flow
 # @Last modified by:    Felix Kramer
-# @Last modified time: 2021-08-30T23:39:31+02:00
+# @Last modified time: 2021-09-08T20:18:02+02:00
 # @License: MIT
 
 import numpy as np
