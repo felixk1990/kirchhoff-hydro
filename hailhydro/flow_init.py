70f5f2594d89825d1ed342d6b4b6b8088941efaa
felix <felixuwekramer@protonmail.com>
2021-11-07 20:40:19 +0100

    fix python layout

--- hailhydro/flow_init.py
+++ hailhydro/flow_init.py
@@ -10,92 +10,92 @@ import numpy as np
 import networkx as nx
 import kirchhoff.circuit_flow as kfc
 
+
 # take an initiliazed circuit and start computing flows
 def initialize_flow_on_circuit(circuit):
 
-    flow_landscape=flow(circuit)
+    flow_landscape = flow(circuit)
 
     return flow_landscape
 
-
 class flow():
 
-    def __init__(self,circuit):
+    def __init__(self, circuit):
 
-        self.circuit=circuit
-        self.B,self.BT=self.circuit.get_incidence_matrices()
+        self.circuit = circuit
+        self.B, self.BT = self.circuit.get_incidence_matrices()
 
-    def find_roots(self,G):
+    def find_roots(self, G):
 
-        roots=[n for n in self.circuit.list_graph_nodes if G.nodes[n]['source']>0]
+        roots = [n for n in self.circuit.list_graph_nodes if G.nodes[n]['source']>0]
 
         return roots
 
-    def find_sinks(self,G):
+    def find_sinks(self, G):
 
-        sinks=[n for n in self.circuit.list_graph_nodes if G.nodes[n]['source']<0]
+        sinks = [n for n in self.circuit.list_graph_nodes if G.nodes[n]['source']<0]
 
         return sinks
 
-    def alpha_omega(self,G,j):
+    def alpha_omega(self, G, j):
 
-        labels=nx.get_edge_attributes(G,'label')
-        for e,label in labels.items():
-            if label==j:
-                alpha=e[1]
-                omega=e[0]
+        labels = nx.get_edge_attributes(G, 'label')
+        for e, label in labels.items():
+            if label == j:
+                alpha = e[1]
+                omega = e[0]
 
-        return alpha,omega
+        return alpha, omega
 
-    def calc_pressure(self,conduct,source):
+    def calc_pressure(self, conduct, source):
 
-        OP=np.dot(self.B,np.dot(np.diag(conduct),self.BT))
-        P,RES,RG,si=np.linalg.lstsq(OP,source,rcond=None)
-        dP=np.dot(self.BT,P)
+        OP = np.dot(self.B, np.dot(np.diag(conduct), self.BT))
+        P, RES, RG, si = np.linalg.lstsq(OP, source, rcond=None)
+        dP = np.dot(self.BT, P)
 
-        return dP, P
+        return dP,  P
 
-    def calc_flow_from_pressure(self,conduct,dP):
+    def calc_flow_from_pressure(self, conduct, dP):
 
-        Q=np.dot(np.diag(conduct),dP)
-
-        return Q
-
-    def calc_flow(self,conduct,source):
-
-        dP,P=self.calc_pressure(conduct,source)
-        Q=np.dot(np.diag(conduct),dP)
+        Q = np.dot(np.diag(conduct), dP)
 
         return Q
 
-    def calc_sq_flow(self,conduct,source):
+    def calc_flow(self, conduct, source):
 
-        dP,P=self.calc_pressure(conduct,source)
-        Q=self.calc_flow_from_pressure(conduct,dP)
+        dP, P = self.calc_pressure(conduct, source)
+        Q = np.dot(np.diag(conduct), dP)
 
-        p_sq=np.multiply(dP,dP)
-        q_sq=np.multiply(Q,Q)
+        return Q
 
-        return p_sq, q_sq
+    def calc_sq_flow(self, conduct, source):
 
-    def calc_cross_section_from_conductivity(self,conductivity,conductance):
+        dP, P = self.calc_pressure(conduct, source)
+        Q = self.calc_flow_from_pressure(conduct, dP)
 
-        R_sq=np.sqrt(conductivity/conductance)
+        p_sq = np.multiply(dP, dP)
+        q_sq = np.multiply(Q, Q)
+
+        return p_sq,  q_sq
+
+    def calc_cross_section_from_conductivity(self, conductivity, conductance):
+
+        R_sq = np.sqrt(conductivity/conductance)
 
         return R_sq
 
-    def calc_conductivity_from_cross_section(self,R_sq,conductance):
+    def calc_conductivity_from_cross_section(self, R_sq, conductance):
 
-        conductivity=np.power(R_sq,2)*conductance
+        conductivity = np.power(R_sq, 2)*conductance
 
         return conductivity
 
     def calc_configuration_flow(self):
 
-        k= self.circuit.edges['conductivity']
-        src= self.circuit.nodes['source']
+        k =  self.circuit.edges['conductivity']
+        src =  self.circuit.nodes['source']
 
-        dP,P=self.calc_pressure(k,src)
-        Q=np.dot(np.diag(k),dP)
+        dP, P = self.calc_pressure(k, src)
+        Q = np.dot(np.diag(k), dP)
 
         return Q
--- hailhydro/flux_random_overflow.py
+++ hailhydro/flux_random_overflow.py
@@ -15,66 +15,64 @@ from hailhydro.flux_init import *
 from hailhydro.flow_random import *
 from hailhydro.flux_overflow import *
 
-class flux_random(overflow ,flow_reroute,object):
+class flux_random(overflow, flow_reroute, object):
 
-    def __init__(self,*args):
-        super(flux_random,self).__init__(args[0])
+    def __init__(self, circuit):
+        super(flux_random, self).__init__(circuit)
 
-    def calc_flow(self,*args):
+    def calc_flow(self, *args):
 
-        graph_matrices=self.get_broken_links_asarray(*args)
-        flow_observables=list(map(self.calc_flows_mapping,graph_matrices))
+        graph_matrices = self.get_broken_links_asarray(*args)
+        flow_observables = list(map(self.calc_flows_mapping, graph_matrices))
 
         return flow_observables
 
-    def calc_transport_observables(self,*args):
+    def calc_transport_observables(self, idx, conduct, flow_observables):
 
         # calc ensemble averages
-        idx,conduct,flow_observables=args
+        graph_matrices = self.get_broken_links_asarray(idx, conduct)
+        R_powers = calc_random_radii(idx, conduct)
+        p_sq = np.power([fo[2] for fo in flow_observables], 2)
 
-        graph_matrices=self.get_broken_links_asarray(idx,conduct)
-        R_powers=calc_random_radii(idx,conduct)
-        p_sq=np.power([fo[2] for fo in flow_observables],2)
+        PHI = list(map(self.calc_noisy_absorption, R_powers[1], flow_observables ) )
+        SHEAR = np.multiply(dV_sq, R_powers[1])
 
-        PHI=list(map( self.calc_noisy_absorption , R_powers[1], flow_observables ) )
-        SHEAR=np.multiply(dV_sq,R_powers[1])
+        avg_shear_sq = np.sum(np.multiply(dV_sq, R_sq), axis = 0)/float(self.num_iteration)
+        avg_PHI = np.mean(PHI, axis = 0)
 
-        avg_shear_sq=np.sum(np.multiply(dV_sq,R_sq),axis=0)/float(self.num_iteration)
-        avg_PHI= np.mean(PHI,axis=0)
-
-        return shear,phi
+        return shear, phi
 
     def calc_noisy_absorption(self, R_sq, flow_observables):
 
-        self.update_transport_matrix(R_sq ,flow_observables)
+        self.update_transport_matrix(R_sq, flow_observables)
 
-        c,B_new=self.solve_absorbing_boundary()
+        c, B_new = self.solve_absorbing_boundary()
 
         return self.calc_absorption(R_sq)
 
-    def update_transport_matrix(self,R_sq,flow_observables):
+    def update_transport_matrix(self, R_sq, flow_observables):
 
         # set peclet number and internal flow state
-        self.circuit.edge['flow_rate']=flow_observables[0]
-        ref_var=self.circuit.scale['length']/self.circuit.scale['diffusion']
+        self.circuit.edge['flow_rate'] = flow_observables[0]
+        ref_var = self.circuit.scale['length']/self.circuit.scale['diffusion']
 
-        V=self.calc_velocity_from_flowrate(self.circuit.edge['flow_rate'],R_sq)
-        self.circuit.edge['peclet']=self.calc_peclet( V,ref_var )
-        A=self.calc_diff_flux( R_sq,1./ref_var )
+        V = self.calc_velocity_from_flowrate(self.circuit.edge['flow_rate'], R_sq)
+        self.circuit.edge['peclet'] = self.calc_peclet(V, ref_var )
+        A = self.calc_diff_flux(R_sq, 1./ref_var )
 
-        x,z,e_up_sinh_x,e_down_sinh_x,coth_x,idx_pack = self.compute_flux_pars()
+        x, z, e_up_sinh_x, e_down_sinh_x, coth_x, idx_pack = self.compute_flux_pars()
 
-        f1= np.multiply(z,A)
-        f2= np.multiply(A,np.multiply(x,coth_x))*0.5
+        f1 = np.multiply(z, A)
+        f2 = np.multiply(A, np.multiply(x, coth_x))*0.5
 
-        f3= np.multiply(np.multiply(A,x),e_up_sinh_x)*0.5
-        f4= np.multiply(np.multiply(A,x),e_down_sinh_x)*0.5
+        f3 = np.multiply(np.multiply(A, x), e_up_sinh_x)*0.5
+        f4 = np.multiply(np.multiply(A, x), e_down_sinh_x)*0.5
 
         # set up concentration_matrix
-        self.B_eff=np.zeros((self.N,self.N))
+        self.B_eff = np.zeros((self.N, self.N))
 
-        for i,n in enumerate(self.circuit.list_graph_nodes):
+        for i, n in enumerate(self.circuit.list_graph_nodes):
 
-            self.B_eff[i,i]= np.sum(  np.add( np.multiply(self.B[i,:],f1),np.multiply(np.absolute(self.B[i,:]),f2))  )
-            self.B_eff[i,self.dict_in[n]]= -f3[self.dict_node_in[n]]
-            self.B_eff[i,self.dict_out[n]]= -f4[self.dict_node_out[n]]
+            self.B_eff[i, i] = np.sum( np.add(np.multiply(self.B[i, :], f1), np.multiply(np.absolute(self.B[i, :]), f2)))
+            self.B_eff[i, self.dict_in[n]] = -f3[self.dict_node_in[n]]
+            self.B_eff[i, self.dict_out[n]] = -f4[self.dict_node_out[n]]
